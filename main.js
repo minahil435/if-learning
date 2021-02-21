a57c9ee06aaa016122f1fd7d14aa406e30d8d0a5
Minahil shafique <minahal.shafique@gmail.com>
2021-02-21 11:36:54 -0500

    if functions

--- main.js
+++ main.js
@@ -3,17 +3,53 @@
  ******************************/
 
 
+function isItLong(myString) {
+  if (myString.length > 20) {
+    return "That's a long string!"
+  }
+}
+
+function isItMedium(myString) {
+  if (myString.length >= 10 && myString.length <= 20) {
+    return "That's a regular sized string!"
+  }
+}
+
+function isItShort(myString) {
+  if (myString.length < 10) {
+    return "That's a small string!"
+  }
+  else {
+    return "That's not a small string!"
+  }
+}
+function howLongIsMyString(myString) {
+  if (myString.length < 10) {
+    return "That's a small string!"
+  }
+  else if (myString.length >= 10 && myString.length <= 20) {
+    return "That's a regular sized string!"
+  }
+  else if (myString.length > 20) {
+    return "That's a long string!"
+  }
+}
 
 
+  function instructorHeight(instructorName) {
+    if (instructorName == "Colin") {return 62}
+    else if (instructorName == "Mesuara"){ return 69}
+    return "I don't know that instructor!"
+  }
 
 
-/**********************************
- *      OUR CODE GOES BELOW.      *
- * THIS IS FOR INTERNAL USE ONLY. *
- * DO NOT ADD OR CHANGE ANYTHING! *
- **********************************/
+        /**********************************
+         *      OUR CODE GOES BELOW.      *
+         * THIS IS FOR INTERNAL USE ONLY. *
+         * DO NOT ADD OR CHANGE ANYTHING! *
+         **********************************/
 
-if (typeof isItLong === 'undefined') {
+        if(typeof isItLong === 'undefined') {
   isItLong = undefined;
 }
 
